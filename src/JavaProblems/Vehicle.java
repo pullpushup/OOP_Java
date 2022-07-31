518037c606241ebb9e24c06afe3cd2b945d44e05
pullpushup <dman1999q@gmail.com>
2022-07-31 10:11:27 +0200

    added Vehicle and its classes 2

--- src/JavaProblems/Vehicle.java
+++ src/JavaProblems/Vehicle.java
@@ -1,4 +1,8 @@
 package JavaProblems;
 
 public class Vehicle {
+    public static void main(String[] args){
+        Outlander outlander = new Outlander(36);
+        outlander.accelerate(30);
+    }
 }
