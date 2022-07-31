8ff142b8c629586a7a4c9db36a02555d9b64cfad
pullpushup <dman1999q@gmail.com>
2022-07-31 22:26:10 +0200

    Encapsulation

--- src/OOPPart2/Encapsulation.java
+++ src/OOPPart2/Encapsulation.java
@@ -1,4 +1,24 @@
 package OOPPart2;
 
 public class Encapsulation {
+    public static void main(String[] args){
+//        Player player = new Player();
+//        player.name= "Sam";
+//        player.health = 25;
+//        player.weapon = "Guns";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health =  " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health =  " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("Ayush",70,"Ar 15");
+        System.out.println("Initial health " + player.getHitPoints());
+
+    }
+
 }
