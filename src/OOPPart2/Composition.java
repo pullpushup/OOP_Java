014768da991cfd9c6abe80cddf75cad7c935ffc2
pullpushup <dman1999q@gmail.com>
2022-07-31 12:01:43 +0200

    added composition again!

--- src/OOPPart2/Composition.java
+++ src/OOPPart2/Composition.java
@@ -2,6 +2,17 @@ package OOPPart2;
 
 public class Composition {
     public static void main(String[] args){
+        Dimensions dimensions = new Dimensions(20,25,5);
+        Case theCase = new Case("55A0","HP","550",dimensions);
 
+        Monitor theMoniter = new Monitor("31 inch","Huawei",31,new Resolution(1920,1080));
+
+        Motherboard theMotherboard= new Motherboard("AB-5000","Intel",2,4,"v2.44");
+
+        PC thePc = new PC(theCase,theMoniter,theMotherboard);
+        thePc.getMonitor().drawPixelAt(1280,720,"red");
+        thePc.getMotherboard().loadProgram("Windows 1.0");
+        thePc.getTheCase().pressPowerButton();
     }
+
 }
