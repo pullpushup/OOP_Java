a48dfc134e7236988db6192dfa25c8dd2d3cffb8
pullpushup <dman1999q@gmail.com>
2022-08-01 17:11:03 +0200

    added update array

--- src/Arrays/MainArrays.java
+++ src/Arrays/MainArrays.java
@@ -2,16 +2,19 @@ package Arrays;
 
 public class MainArrays {
     public static void main(String[] args){
-        int[] myVariable = new int[10];// {1,2,3,4,5,6,7,8,9,10};;
+        int[] myVariable = new int[25];// {1,2,3,4,5,6,7,8,9,10};;
 //        myVariable[0] = 45;
 //        myVariable[1] = 476;
 //        myVariable[5] = 50;
 
-        for(int i=0; i<10; i++){
+        for(int i=0; i<myVariable.length; i++){
             myVariable[i] = i*10;
         }
-        for(int i =0; i<10; i++) {
-            System.out.println("Element " + i + ", value is" +myVariable[i] );
+        printArray(myVariable);
+    }
+    public static void printArray(int[] array){
+        for (int i=0; i<array.length; i++) {
+            System.out.println("Element " + i + ", value is " + array[i]);
         }
     }
 }
