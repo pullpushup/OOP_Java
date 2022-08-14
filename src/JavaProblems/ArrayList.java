005749e3df75444898cf36d31700f05ed8a6f826
pullpushup <dman1999q@gmail.com>
2022-08-14 17:22:18 +0200

    added arraylist#1

--- src/JavaProblems/ArrayList.java
+++ src/JavaProblems/ArrayList.java
@@ -23,10 +23,77 @@ public class ArrayList {
                 case 1:
                     mobilePhone.printContacts();
                     break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                     queryContact();
+                     break;
+                case 6:
+                    printActions();
+                    break;
             }
         }
     }
 
+    private static void addNewContact(){
+        System.out.println("Enter new contact name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phone = scanner.nextLine();
+        ArrayListContact newContact = ArrayListContact.createContact(name,phone);
+        if(mobilePhone.addNewContact(newContact)){
+            System.out.println("New contact added: "+ name + ", phone = "+ phone);
+        }else {
+            System.out.println("Cannot add, " + name + " already on file.");
+        }
+    }
+    private static void updateContact(){
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        ArrayListContact existingContactRecord = mobilePhone.queryContact(name);
+        if(existingContactRecord == null){
+            System.out.println("Contact not found.");
+            return;
+        }
+
+        System.out.println("Enter new contact name: ");
+        String newName= scanner.nextLine();
+        System.out.println("Enter new contact phone number = ");
+        String newNumber = scanner.nextLine();
+        ArrayListContact newContact = ArrayListContact.createContact(newName,newNumber);
+        if(mobilePhone.UpdateContact(existingContactRecord,newContact)){
+            System.out.println("Successfully updated record");
+        }else{
+            System.out.println("Error updating record.");
+        }
+    }
+    private static void removeContact(){
+            System.out.println("Enter existing  contact name: ");
+            String name = scanner.nextLine();
+            ArrayListContact existingContactRecord = mobilePhone.queryContact(name);
+            if(existingContactRecord == null){
+                System.out.println("Contact not found.");
+                return;
+            }
+        System.out.println("Enter new contact name: ");
+        String newName = scanner.nextLine();
+        System.out.println("Enter new contact phone number: ");
+        String newNumber = scanner.nextLine();
+        ArrayListContact newContact = ArrayListContact.createContact(newName,newNumber);
+        if (mobilePhone.removeContact(existingContactRecord)){
+            System.out.println("Successfully deleted");
+        } else {
+            System.out.println("Error updating record.");
+        }
+
+    }
 
     private static void startPhone(){
         System.out.println("Starting phone....");
